f795cb885dd95c87b779e73141d5363a80f157c2
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-28 01:36:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,31 @@
 class Dog
-
-  attr_accessor :name
-
   @@all = []
 
   def initialize(name)
     @name = name
-    @@all << self
+    self.save
   end
 
   def self.all
-    @@all.each do |dog|
-      puts dog.name
-    end
+    @@all
   end
 
   def self.clear_all
-    @@all.clear
+    @@all = [] 
+  end
+
+  def self.print_all
+    @@all.each do |dog|
+      puts dog.name
+    end
+end
+
+  def save
+    @@all << self
+  end
+
+  def name
+    @name
   end
 
 end
